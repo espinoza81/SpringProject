55567b5a1e66b2e74e542904bb85f704eda8d233
Bonka Tsvetanova <espinoza81@yahoo.com>
2023-03-18 08:49:53 +0200

    Delete DogPark/src/main directory

--- DogPark/src/main/java/dogpark/exeption/NoEnoughResourceException.java
+++
@@ -1,7 +0,0 @@
-package dogpark.exeption;
-
-public class NoEnoughResourceException extends RuntimeException{
-    public NoEnoughResourceException(String messages) {
-        super(messages);
-    }
-}
--- DogPark/src/main/java/dogpark/repository/BreedRepository.java
+++
@@ -1,9 +0,0 @@
-package dogpark.repository;
-
-import dogpark.model.entity.BreedEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BreedRepository extends JpaRepository<BreedEntity, Long> {
-}
