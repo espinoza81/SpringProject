e0d15e3bd68e6a3c0b401f5d1e542af6e5ab53c8
Bonka Tsvetanova <espinoza81@yahoo.com>
2023-04-02 20:59:16 +0300

    Update InterceptorConfiguration.java

--- DogPark/src/main/java/dogpark/config/InterceptorConfiguration.java
+++ DogPark/src/main/java/dogpark/config/InterceptorConfiguration.java
@@ -1,4 +1,22 @@
 package dogpark.config;
 
-public class InterceptorConfiguration   {
+import dogpark.interceptor.LoggingInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfiguration implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    public InterceptorConfiguration(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
 }
